ec8d9d5a800ce5cc2fde6dddb6106e356b4106dd
dcwjxa <3344053481247475-dcwjxa@noreply.coze.cn>
2026-01-17 20:22:26 +0800

    package: 上传亮色主题压缩包

---
+++ upload_light.py
@@ -0,0 +1,22 @@
+import os
+from coze_coding_dev_sdk.s3 import S3SyncStorage
+
+storage = S3SyncStorage(
+    endpoint_url=os.getenv("COZE_BUCKET_ENDPOINT_URL"),
+    access_key="",
+    secret_key="",
+    bucket_name=os.getenv("COZE_BUCKET_NAME"),
+    region="cn-beijing",
+)
+
+file_path = "/workspace/projects/tea-house-light-theme.tar.gz"
+
+with open(file_path, "rb") as f:
+    key = storage.stream_upload_file(
+        fileobj=f,
+        file_name="tea-house-light-theme.tar.gz",
+        content_type="application/gzip",
+    )
+
+download_url = storage.generate_presigned_url(key=key, expire_time=3600)
+print(download_url)
